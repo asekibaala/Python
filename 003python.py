959beb9034162d8f0c66bda5ac68e6ccd692ed1f
asekibaala <54054280+asekibaala@users.noreply.github.com>
2023-06-03 07:41:14 +0300

    functions

---
+++ 003python.py
@@ -0,0 +1,19 @@
+def operate(operation,x,y):
+    if operation == 'sum':
+        return x + y
+    elif operation == 'subtract':
+        return x - y
+    elif operation == 'multiply':
+        return x * y
+    elif operation == 'divide':
+        return x / y
+    
+print("==== Sum ====")
+print(operate('sum',2,3,))
+
+print("==== Subtract ====")
+print(operate('subtract',2,5))
+print(operate('subtract',5,2))
+
+print("==== Multiply ====")
+print(operate('multiply',2,3))
