caf00700f83b6742266a3d848ceac9d6daf04630
asekibaala <54054280+asekibaala@users.noreply.github.com>
2023-06-04 08:17:49 +0300

    string formatting

--- 005Python.py
+++ 005Python.py
@@ -5,3 +5,11 @@ print(my_name)
 print(my_name[7])
 print(my_name[4:9])
 print(my_name[-6])
+
+subject = "welcom {}!"
+print(subject.format(my_name))
+
+today = "Today is {}, {}, {}"
+msg = today.format("Sunday", "June", 4)
+
+print(msg)
