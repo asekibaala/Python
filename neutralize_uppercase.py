645c51155d77681bee52f2ab04f661914fadd49a
asekibaala <54054280+asekibaala@users.noreply.github.com>
2023-06-16 06:25:48 +0300

    No Uppercase Allowed

---
+++ neutralize_uppercase.py
@@ -0,0 +1,7 @@
+def neutralize_uppercase(stringy):
+    count = 0
+    while count < len(stringy):
+        if stringy[count] == stringy[count].upper():
+            return ""
+        count += 1
+    return stringy
