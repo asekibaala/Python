23a73cc89b277bfd08eb54776a5fd22bfbee460d
asekibaala <54054280+asekibaala@users.noreply.github.com>
2023-07-08 06:18:20 +0300

    update comments

---
+++ add_to_dictionary.py
@@ -0,0 +1,15 @@
+def add_to_dictionary(a_dictionary):
+    a_dictionary['fav_language'] = 'python'
+    return a_dictionary
+
+"""
+function that receives a dictionary as input, adds a 
+new key-value pair saying "fav_language": "python" to it,
+ and returns it! :D
+
+# {
+#   "first_name": "jon",
+#   "last_name": "snow",
+#   "fav_language": "python"
+# }
+"""
